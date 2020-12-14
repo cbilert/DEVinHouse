89d9525eb0aa4dc40c295dd65f3467c04811a470
Cristian <cristian.bilert@celk.net>
2020-12-14 18:40:45 -0300

    s4 e3

---
+++ semana 4/exercicio3.js
@@ -0,0 +1,32 @@
+let exercicio3 = document.getElementById('exercicio3');
+const pessoas = [
+    {
+      nome: 'Vampiro',
+      idade: 900,
+      telefone: 48987465432,
+      profissao: 'Governante'
+    },
+    {
+      tipo: 'Lobisomem',
+      idade: 80,
+      telefone: 48987465432,
+      profissao: 'Governante'
+    },
+    {
+      tipo: 'Ogro',
+      idade: 4,
+      telefone: 48987465432,
+      profissao: 'Governante'
+    }
+  ]
+
+function executar(){    
+    document.getElementById('resultado').value = buscaMaiorDeIdade(pessoas);
+}
+
+const buscaMaiorDeIdade = pess => pessoas.filter(pessoa => pessoa.idade >= 18);
+
+
+
+
+exercicio3.addEventListener('click', executar);
