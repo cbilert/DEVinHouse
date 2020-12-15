31b6bfa34cd1a268bf8cca41b260e7f8c5967a19
Cristian <cristian.bilert@celk.net>
2020-12-14 21:53:04 -0300

    S4 E13

---
+++ semana 4/exercicio13.js
@@ -0,0 +1,12 @@
+let exercicio13 = document.getElementById('exercicio13');
+
+let cids = JSON.parse('[{"cidade":"Cidade1", "uf":"SC"},{"cidade":"Cidade2", "uf":"PR"},{"cidade":"Cidade3", "uf":"SC"},{"cidade":"Cidade4", "uf":"SP"}]');
+
+function executar(){    
+    document.getElementById('resultado').value = JSON.stringify(buscaPorUF(cids,'SC'));
+    console.log(buscaPorUF(cids,'SC'));
+}
+
+const buscaPorUF = (cidades,uf) => cidades.filter(cidade => cidade.uf === uf);
+
+exercicio13.addEventListener('click', executar);
